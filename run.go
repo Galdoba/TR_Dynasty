a74155c58ec174902061562cc4726d309fd985af
Galdoba <galdoba@mail.ru>
2020-07-21 14:12:20 +1000

    test

--- run.go
+++ run.go
@@ -1,35 +1,12 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/Galdoba/TR_Dynasty/profile"
-
-	"github.com/Galdoba/TR_Dynasty/constant"
-	"github.com/Galdoba/TR_Dynasty/world"
 	"github.com/Galdoba/utils"
 )
 
 func main() {
 	utils.RandomSeed()
-
-	world1 := world.NewWorld("Arnora")
-	world1.SetValue(constant.PrAtmo, "5")
-	trueUwp := profile.Compose("UWP", &world1)
-	fmt.Println(trueUwp)
-	uwp := profile.RandomUWP()
-	fmt.Println(uwp)
-	world1.MergeUWP(uwp)
-	fmt.Println(world1, world1.UWP())
-	fmt.Println("------------------")
-	trueUwp = profile.Compose("UWP", &world1)
-	fmt.Println(trueUwp)
-	fmt.Println("-----------------")
-	fmt.Println("-----------------")
-	world1.SecondSurvey()
-	fmt.Println(world1)
-	// //system.AllNames("Sol")
-
+	//sd
 	//SER
 }
 
