063873148b27bd635a327330dd4aa2448dc367e2
Galdoba <galdoba@mail.ru>
2021-01-15 19:21:51 +1000

    test

--- run.go
+++ run.go
@@ -1,8 +1,15 @@
 package main
 
-import "github.com/Galdoba/TR_Dynasty/starsystem"
+import (
+	"fmt"
+
+	"github.com/Galdoba/TR_Dynasty/starsystem"
+	"github.com/Galdoba/TR_Dynasty/wrld"
+)
 
 func main() {
+	w := wrld.PickWorld()
+	fmt.Println(w)
 
 	starsystem.Test()
 
@@ -41,6 +48,36 @@ func main() {
 
 	//autoGM.AutoGM()
 }
+
+/*
+B34+3
+11 - Resorses
+3 - Labor
+4 - Infrastructure
+3 - Culture
+
+Base Demand: 11
+Total Demand: 4   [2d6-1-2]
+
+Export: 11-4 = 7
+Export Benefit: 0.5
+
+Resource Available := 3.5 + 4 = 7.5
+
+Labor Base = 0.0001 * 7
+
+GWP = ((14*0.1*7.5)*(0.0001*7)*4) / (3+1)
+GWP = 10.5 * 0.0007 * 4 / 4
+GWP = 0.007 RU
+
+Trading Finished Goods = 1.106
+Aggregated Demand = 1.0
+
+Final GWP = 0.008 RU
+
+
+
+*/
 
 //OB Ia Ia Ia II II II II
 /*
