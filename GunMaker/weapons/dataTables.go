01a9055f46acbdb6f2190634d2c94612d3dd6d36
Galdoba <galdoba@mail.ru>
2021-05-24 14:19:21 +1000

    11

--- GunMaker/weapons/dataTables.go
+++ GunMaker/weapons/dataTables.go
@@ -65,3 +65,69 @@ func rangeWeapons() [][]string {
 	}
 	return data
 }
+
+func weaponsDiscriptors() [][]string {
+	data := [][]string{
+		[]string{"Category    | Code | Descriptor   | TL | R= | Mass | qreBs | H2      | D2    | H3     | D3    | Hits(v1) | Cr"},
+		[]string{"Artillery   | aF   | Anti-Flyer   | +4 | =6 | x6.0 |       | Frag    | 1     | Blast  | 3     | 4        | x3.0"},
+		[]string{"Artillery   | aT   | Anti-Tank    |    | =5 | x8.0 |       | Pen     | 3     | Blast  | 3     | 6        | x2.0"},
+		[]string{"Artillery   | A    | Assault      | +2 | =4 | x0.8 |       | Bang    | 1     | Blast  | 2     | 3        | x1.5"},
+		[]string{"Artillery   | F    | Fusion       | +7 | =4 | x2.3 |       | Pen     | 4     | Burn   | 4     | 8        | x6.0"},
+		[]string{"Artillery   | G    | Gauss        | +7 | =4 | x0.9 |       | Bullet  | 3     |        |       | 3        | x2.0"},
+		[]string{"Artillery   | P    | Plasma       | +5 | =4 | x2.5 |       | Pen     | 3     | Burn   | 3     | 6        | x2.0"},
+		[]string{"Long Guns   |      | (blank)      |    |    |      |       |         |       |        |       |          | x1.0"},
+		[]string{"Long Guns   | Ac   | Accelerator  | +4 |    | x0.6 |       | Bullet  | 2     |        |       | 2        | x3.0"},
+		[]string{"Long Guns   | A    | Assault      | +2 | =4 | x0.8 |       | Blast   | 2     | Bang   | 1     | 3        | x1.5"},
+		[]string{"Long Guns   | B    | Battle       | +1 | =5 | x1.0 | +1    | Bullet  | 1     |        |       | 1        | x0.8"},
+		[]string{"Long Guns   | C    | Combat       | +2 | =3 | x0.9 |       | Frag    | 2     |        |       | 2        | x1.5"},
+		[]string{"Long Guns   | D    | Dart         | +1 | =4 | x0.6 |       | Tranq   | 1-2-3 |        |       | 1-2-3    | x0.9"},
+		[]string{"Long Guns   | P    | Poison Dart  | +1 | =4 | x1.0 |       | Poison  | 1-2-3 |        |       | 1-2-3    | x0.9"},
+		[]string{"Long Guns   | G    | Gauss        | +7 |    | x0.9 |       | Bullet  | 3     |        |       | 3        | x2.0"},
+		[]string{"Long Guns   | H    | Hunting      |    | =3 | x0.9 | -1    | Bullet  | 1     |        |       | 1        | x1.2"},
+		[]string{"Long Guns   | L    | Laser        | +5 |    | x1.2 |       | Burn    | 2     | Pen    | 2     | 4        | x6.0"},
+		[]string{"Long Guns   | Sp   | Splat        | +2 | =4 | x1.3 | +1    | Bullet  | 1     |        |       | 1        | x2.4"},
+		[]string{"Long Guns   | S    | Survival     |    | =2 | x0.5 |       | Bullet  | 1     |        |       | 1        | x1.2"},
+		[]string{"Handguns    |      | (blank)      |    |    |      |       |         |       |        |       |          | x1.0"},
+		[]string{"Handguns    | Ac   | Accelerator  | +4 |    | x0.6 |       | Bullet  | 2     |        |       | 2        | x3.0"},
+		[]string{"Handguns    | L    | Laser        | +5 |    | x1.2 |       | Burn    | 2     | Pen    | 2     | 4        | x2.0"},
+		[]string{"Handguns    | M    | Machine      |    | =2 | x1.2 |       | Bullet  | 2     |        |       | 2        | x1.5"},
+		[]string{"Shotguns    |      | (blank)      |    |    |      |       |         |       |        |       |          | x1.0"},
+		[]string{"Shotguns    | A    | Assault      | +2 | =4 | x0.8 |       | Bang    | 1     | Blast  | 2     | 3        | x2.0"},
+		[]string{"Shotguns    | H    | Hunting      |    | =3 | x0.9 |       | Bullet  | 1     |        |       | 1        | x1.2"},
+		[]string{"Machineguns |      | (blank)      |    |    |      |       |         |       |        |       |          | x1.0"},
+		[]string{"Machineguns | aF   | Anti-Flyer  +| 4  | =6 | x6.0 |       | Frag    | 1     | Blast  | 3     | 4        | x3.0"},
+		[]string{"Machineguns | A    | Assault     +| 2  | =4 | x0.8 |       | Bang    | 1     | Blast  | 2     | 3        | x1.5"},
+		[]string{"Machineguns | S    | Sub         -| 1  | =3 | x0.3 |       | Bullet  | -1    |        |       | -1       | x0.9"},
+		[]string{"Spray       | A    | Acid         |    | =3 | x1.0 | +1    | Corrode | 2     | Pen    | 1-2-3 | 4        | x3.0"},
+		[]string{"Spray       | H    | Fire         |    | =1 | x0.9 |       | Burn    | 1-2-3 | Pen    | 1-2-3 | 2-4-6    | x2.0"},
+		[]string{"Spray       | P    | Poison Gas   |    | =2 | x1.0 |       | Gas     | 1-2-3 | Poison | 1-2-3 | 2-4-6    | x3.0"},
+		[]string{"Spray       | S    | Stench      +| 3  | =2 | x0.4 |       | Stench  | 1-2-3 |        |       | 1-2-3    | x1.2"},
+		[]string{"Launchers   | aF   | AF Missile  +| 4  | =7 | x4.0 |       | Frag    | 2     | Blast  | 3     | 5        | x3.0"},
+		[]string{"Launchers   | aT   | AT Missile  +| 3  | =4 | x1.0 | +1    | Frag    | 2     | Pen    | 3     | 5        | x2.0"},
+		[]string{"Launchers   | Gr   | Grenade     +| 1  | =4 | x0.8 |       | Frag    | 2     | Blast  | 2     | 4        | x1.0"},
+		[]string{"Launchers   | M    | Missile     +| 1  | =6 | x2.2 |       | Frag    | 2     | Pen    | 2     | 4        | x5.0"},
+		[]string{"Launchers   | RAM  | RAM Grenade +| 2  | =6 | x1.0 |       | Frag    | 2     | Blast  | 2     | 4        | x3.0"},
+		[]string{"Launchers   | R    | Rocket      -| 1  | =5 | x3.0 |       | Frag    | 2     | Pen    | 2     | 4        | x1.0"},
+	}
+	return data
+}
+
+func weaponBurden() [][]string {
+	data := [][]string{
+		[]string{"Category Code Descriptor      TL  R= Mass qreBs Misc D1 D2 Comment                D3 Cr"},
+		[]string{"Burden        (blank)         0   0  x1.0 0             0                            x1.0"},
+		[]string{"Burden   aD   Anti-Designator +3  +1 x3.0 +3 1             Not Pistols. Shotguns.    x 3.0"},
+		[]string{"Burden   B    Body            +2  =1 x0.5 -4 -1 Only Pistols. x 3.0"},
+		[]string{"Burden   D    Disposable      +3  0 x0.9 -1 Q= -2 0 x 0.5"},
+		[]string{"Burden   H    Heavy           0   +1 x1.3 +3 1 Not Laser x 2.0"},
+		[]string{"Burden   Lt   Light           0   -1 x0.7 -1 -1 Not Laser x 1.1"},
+		[]string{"Burden   M    Magnum          +1  +1 x1.1 +1 1 Only Pistols. x 1.1"},
+		[]string{"Burden   M    Medium          0   0 x1.0 0 0 Not Pistols. x 1.0"},
+		[]string{"Burden   R    Recoilless      +1  -1 x1.2 0 1 x 3.0"},
+		[]string{"Burden   Sn   Snub            +1  =2 x0.7 -3 1 x 1.5"},
+		[]string{"Burden   Vh   Vheavy          0 +5 x4.0 +4 5 x 5.0"},
+		[]string{"Burden   Vl   Vlight          +1 -2 x0.6 -2 -1 x 2.0"},
+		[]string{"Burden   Vrf  VRF             +2 0 x14.0 +5 1 Only Guns and MGs x 9.0"},
+	}
+	return data
+}
