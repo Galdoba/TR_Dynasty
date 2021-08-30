6acec4a8cd9130b7bb0a10bdd6e8298aa8544157
Galdoba <galdoba@mail.ru>
2021-08-30 14:00:19 +1000

    dice

--- T5/assets/characteristics.go
+++ T5/assets/characteristics.go
@@ -1,5 +1,7 @@
 package assets
 
+import "fmt"
+
 const (
 	C1 = iota
 	C2
@@ -28,14 +30,39 @@ const (
 )
 
 type Characteristic struct {
-	char     int
-	alias    string
-	charDice int //сколько кубов ролится для создания х-ки
-	err      error
+	value        int
+	genetics     int
+	positionCode int
+	alias        string // not needed. TODO: get alias via code
+	charDice     int    //сколько кубов ролится для создания х-ки
+	err          error
 }
 
-func NewCharacteristic(code int, dice ...int) *Characteristic {
+func NewCharacteristic(code int, dice int) *Characteristic {
 	c := Characteristic{}
+	switch code {
+	default:
+		c.err = fmt.Errorf("code '%v' is unknown", code)
+	case Strength:
+		c.positionCode = C1
+	case Dexterity, Agility, Grace:
+		c.positionCode = C2
+	case Endurance, Stamina, Vigor:
+		c.positionCode = C3
+	case Intelligence:
+		c.positionCode = C4
+	case Education, Training, Instinct:
+		c.positionCode = C5
+	case Social, Charisma, Caste:
+		c.positionCode = C6
+	case Psionics:
+		c.positionCode = CP
+	case Sanity:
+		c.positionCode = CS
+	}
+	for i := 0; i < dice; i++ {
+		c.value = dice.Roll1D()
+	}
 	// 649 8 12
 	// 165 +0 +2
 	//  84 +2 +8
