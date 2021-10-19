1cfc9732d8b4e8e3c5e2342055190ed2704b989b
Galdoba <galdoba@mail.ru>
2021-10-19 19:17:18 +1000

    all1

---
+++ pkg/universe/survey/calculations/allegiance.go
@@ -0,0 +1,256 @@
+package calculations
+
+
+
+Code	Pre-T5	Description	Location
+3EoG	Ga	Third Empire of Gashikan	Mend/Gash/Tren
+4Wor	Fw	Four Worlds	Farf
+AkUn	Ak	Akeena Union	Gate
+AlCo	Al	Altarean Confederation	Vang
+AnTC	Ac	Anubian Trade Coalition	Hint
+AsIf	As	Iyeaao'fte	Ustr
+AsMw	As	Aslan Hierate, single multiple-world clan dominates	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr/Verg
+AsOf	As	Oleaiy'fte	Ustr
+AsSc	As	Aslan Hierate, multiple clans split control	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsSF	As	Aslan Hierate, small facility	
+AsT0	A0	Aslan Hierate, Tlaukhu control, Yerlyaruiwo (1), Hrawoao (13), Eisohiyw (14), Ferekhearl (19)	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsT1	A1	Aslan Hierate, Tlaukhu control, Khauleairl (2), Estoieie' (16), Toaseilwi (22)	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsT2	A2	Aslan Hierate, Tlaukhu control, Syoisuis (3)	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsT3	A3	Aslan Hierate, Tlaukhu control, Tralyeaeawi (4), Yulraleh (12), Aiheilar (25), Riyhalaei (28)	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsT4	A4	Aslan Hierate, Tlaukhu control, Eakhtiyho (5), Eteawyolei' (11), Fteweyeakh (23)	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsT5	A5	Aslan Hierate, Tlaukhu control, Hlyueawi (6), Isoitiyro (15)	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsT6	A6	Aslan Hierate, Tlaukhu control, Uiktawa (7), Iykyasea (17), Faowaou (27)	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsT7	A7	Aslan Hierate, Tlaukhu control, Ikhtealyo (8), Tlerfearlyo (20), Yehtahikh (24)	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsT8	A8	Aslan Hierate, Tlaukhu control, Seieakh (9), Akatoiloh (18), We'okunir (29)	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsT9	A9	Aslan Hierate, Tlaukhu control, Aokhalte (10), Sahao' (21), Ouokhoi (26)	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsTA	Ta	Tealou Arlaoh	Uist/Ustr
+AsTv	As	Aslan Hierate, Tlaukhu vassal clan dominates	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsTz	As	Aslan Hierate, Zodia clan	Iwah
+AsVc	As	Aslan Hierate, vassal clan dominates	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsWc	As	Aslan Hierate, single one-world clan dominates	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AsXX	As	Aslan Hierate, unknown	Akti/Dark/Eali/Hlak/Iwah/Reav/Rift/Stai/Troj/Uist/Ustr
+AvCn	Ac	Avalar Consulate	
+BaCl	Bc	Backman Cluster	
+Bium	Bi	The Biumvirate	Farf
+BlSo	Bs	Belgardian Sojurnate	Troj
+BoWo	Bw	Border Worlds	
+CaAs	Cb	Carrillian Assembly	Reav
+CaPr	Ca	Principality of Caledon	Reav
+CaTe	Ct	Carter Technocracy	Reav
+CoAl	Ca	Corsair Alliance	
+CoBa	Ba	Confederation of Bammesuka	Mend
+CoLg	CL	Corellan League	Beyo/Vang
+CoLp	Lp	Council of Leh Perash	Hint
+CRAk	CA	Anakudnu Cultural Region	
+CRGe	CG	Geonee Cultural Region	
+CRSu	CS	Suerrat Cultural Region	
+CRVi	CV	Vilani Cultural Region	
+CsCa	Ca	Client state, Principality of Caledon	Reav
+CsHv	Hc	Client state, Hive Federation	Cruc/Spic
+CsIm	Cs	Client state, Third Imperium	various
+CsMo	Cm	Client state, Duchy of Mora	Spin
+CsRr	Cr	Client state, Republic of Regina	Spin
+CsTw	KC	Client state, Two Thousand Worlds	various
+CsZh	Cz	Client state, Zhodani Consulate	Spin/Troj
+CyUn	Cu	Cytralin Unity	Hint
+DaCf	Da	Darrian Confederation	Spin
+DeHg	Dh	Descarothe Hegemony	Farf
+DeNo	Dn	Demos of Nobles	Newo
+DiGr	Dg	Dienbach Grüpen	Newo
+DoAl	Az	Domain of Alntzar	Farf
+DuCf	Cd	Confederation of Duncinae	Reav
+DuMo	Mo	Duchy of Mora	Spin
+EsMa	Es	Eslyat Magistracy	Vang
+FdAr	Fa	Federation of Arden	
+FdDa	Fd	Federation of Daibei	
+FdIl	Fi	Federation of Ilelish	
+FeAl	Fa	Federation of Alsas	Farf
+FeAm	FA	Federation of Amil	Cruc
+FeHe	Fh	Federation of Heron	Glim
+FlLe	Fl	Florian League	Troj
+GaFd	Ga	Galian Federation	Gate
+GaRp	Gr	Gamma Republic	Glim
+GdKa	Rm	Grand Duchy of Kalradin	Cruc
+GdMh	Ma	Grand Duchy of Marlheim	Reav
+GdSt	Gs	Grand Duchy of Stoner	Glim
+GeOr	Go	Gerontocracy of Ormine	Dark
+GlEm	Gl	Glorious Empire	Troj
+GlFe	Gf	Glimmerdrift Federation	Cruc/Glim
+GnCl	Gi	Gniivi Collective	Hint
+HaCo	Hc	Haladon Cooperative	Farf
+HeCo	HC	Hefrin Colony	Beyo
+HoPA	Ho	Hochiken People's Assembly	Gate
+HvFd	Hv	Hive Federation	Spic
+HyLe	Hy	Hyperion League	Vang
+IHPr	IS	I'Sred*Ni Protectorate	Beyo
+ImAp	Im	Third Imperium, Amec Protectorate	Dagu
+ImDa	Im	Third Imperium, Domain of Antares	Anta/Empt/Lish
+ImDc	Im	Third Imperium, Domain of Sylea	Core/Delp/Forn/Mass
+ImDd	Im	Third Imperium, Domain of Deneb	Dene/Reft/Spin/Troj
+ImDg	Im	Third Imperium, Domain of Gateway	Glim/Hint/Ley
+ImDi	Im	Third Imperium, Domain of Ilelish	Daib/Ilel/Reav/Verg/Zaru
+ImDs	Im	Third Imperium, Domain of Sol	Alph/Dias/Magy/Olde/Solo
+ImDv	Im	Third Imperium, Domain of Vland	Corr/Dagu/Gush/Reft/Vlan
+ImLa	Im	Third Imperium, League of Antares	Anta
+ImLc	Im	Third Imperium, Lancian Cultural Region	Corr/Dagu/Gush
+ImLu	Im	Third Imperium, Luriani Cultural Association	Ley/Forn
+ImSy	Im	Third Imperium, Sylean Worlds	Core
+ImVd	Ve	Third Imperium, Vegan Autonomous District	Solo
+IsDo	Id	Islaiat Dominate	Eali
+JAOz	Jo	Julian Protectorate, Alliance of Ozuvon	Mend
+JaPa	Ja	Jarnac Pashalic	Beyo/Vang
+JAsi	Ja	Julian Protectorate, Asimikigir Confederation	Amdu/Mend
+JCoK	Jc	Julian Protectorate, Constitution of Koekhon	Amdu/Mend
+JHhk	Jh	Julian Protectorate, Hhkar Sphere	Amdu/Mend
+JLum	Jd	Julian Protectorate, Lumda Dower	Mend
+JMen	Jm	Julian Protectorate, Commonwealth of Mendan	Mend/Gash
+JPSt	Jp	Julian Protectorate, Pirbarish Starlane	Mend
+JRar	Vw	Julian Protectorate, Rar Errall/Wolves Warren	Mend
+JuHl	Hl	Julian Protectorate, Hegemony of Lorean	Amdu/Empt/Mend
+JUkh	Ju	Julian Protectorate, Ukhanzi Coordinate	Mend
+JuNa	Jn	Jurisdiction of Nadon	Cano
+JuPr	Jp	Julian Protectorate	Amdu/Empt/Mend
+JuRu	Jr	Julian Protectorate, Rukadukaz Republic	Empt/Mend
+JVug	Jv	Julian Protectorate, Vugurar Dominion	Mend
+KaCo	KC	Katowice Conquest	Cruc
+KaEm	KE	Katanga Empire	Beyo
+KaTr	Kt	Kajaani Triumverate	Vang
+KaWo	KW	Karhyri Worlds	Cruc
+KhLe	Kl	Khuur League	Ley
+KkTw	Kk	Two Thousand Worlds	various
+KoEm	Ko	Korsumug Empire	Thet
+KoPm	Pm	Percavid Marches	Thet
+KPel	Pe	Kingdom of Peladon	Thet
+KrPr	Kr	Krotan Primacy	Gzir/Rice/KaaG
+LeSu	Ls	League of Suns	Farf
+LnRp	Ln	Loyal Nineworlds Republic	Glim
+LuIm	Li	Lucan's Imperium	
+LyCo	Ly	Lanyard Colonies	Reav
+MaCl	Ma	Mapepire Cluster	Beyo
+MaEm	Mk	Maskai Empire	Glim
+MaSt	Ma	Maragaret's Domain	
+MaUn	Mu	Malorn Union	Cano/Alde
+MeCo	Me	Megusard Corporate	Gate
+MiCo	Mi	Mische Conglomerate	Cruc
+MnPr	Mn	Mnemosyne Principality	Farf
+MoLo	ML	Monarchy of Lod	Beyo
+MrCo	MC	Mercantile Concord	Cruc
+NaAs	As	Non-Aligned, Aslan-dominated	Akti/Dark/Eali/Rift/Uist/Ustr
+NaCh	Na	Non-Aligned, TBD	Sidi
+NaDr	Dr	Non-Aligned, Droyne-dominated	various
+NaHu	Na	Non-Aligned, Human-dominated	various
+NaVa	Va	Non-Aligned, Vargr-dominated	various
+NaXX	Na	Non-Aligned, unclaimed	various
+NkCo	NC	Nakris Confederation	Beyo
+OcWs	Ow	Outcasts of the Whispering Sky	Hint
+OlWo	Ow	Old Worlds	Cruc
+PlLe	Pl	Plavian League	Gate
+PrBr	PB	Principality of Bruhkarr	Beyo
+Prot	Pt	The Protectorate	Farf
+RaRa	Ra	Ral Ranta	Hint
+Reac	Rh	The Reach	Cruc
+ReUn	Re	Renkard Union	Gate
+SaCo	Sc	Salinaikin Concordance	Farf
+Sark	Sc	Sarkan Constellation	Mend
+SeFo	Sf	Senlis Foederate	Troj
+SELK	Lk	Sha Elden Lith Kindriu	Gzir/KaaG
+ShRp	SR	Stormhaven Republic	Beyo
+SlLg	Sl	Shukikikar League	Glim
+SoBF	So	Solomani Confederation, Bootean Federation	Solo
+SoCf	So	Solomani Confederation	Alph/Diab/Dark/Hint/Magy/Olde/Reav/Solo/Spic/Ustr
+SoCT	So	Solomani Confederation, Consolidation of Turin	Alph
+SoFr	Fr	Solomani Confederation, Third Reformed French Confederate Republic	Alde
+SoHn	Hn	Solomani Confederation, Hanuman Systems	Lang
+SoKv	Kv	Solomani Confederation, Kostov Confederate Republic	Newo
+SoNS	So	Solomani Confederation, New Slavic Solidarity	Magy
+SoQu	Qu	Solomani Confederation, Grand United States of Quesada	Alde
+SoRD	So	Solomani Confederation, Reformed Dootchen Estates	Magy
+SoRz	So	Solomani Confederation, Restricted Zone	Alde/Newo
+SoWu	So	Solomani Confederation, Wuan Technology Association	Diab/Magy
+StCl	Sc	Strend Cluster	Troj
+StIm	St	Strephon's Worlds	
+SwCf	Sw	Sword Worlds Confederation	Spin
+SwFW	Sw	Swanfei Free Worlds	Gate
+SyRe	Sy	Syzlin Republic	Cruc
+TeCl	Tc	Tellerian Cluster	Vang
+TrBr	Tb	Trita Brotherhood	Cano
+TrCo	Tr	Trindel Confederacy	Gate
+TrDo	Td	Trelyn Domain	Vang/Farf
+TroC	Tr	Trooles Confederation	Thet
+UnGa	Ug	Union of Garth	Farf
+UnHa	Uh	Union of Harmony	Dark/Reav
+V17D	V7	17th Disjucture	Mesh/Wind
+V40S	Ve	40th Squadron	Gvur
+VA16	V6	Assemblage of 1116	
+VAkh	VA	Akhstuti	Tugl
+VAnP	Vx	Antares Pact	Mesh/Mend
+VARC	Vr	Anti-Rukh Coalition	Gvur
+VAsP	Vx	Ascendancy Pact	Knoe
+VAug	Vu	United Followers of Augurgh	Dene/Tugl
+VBkA	Vb	Bakne Alliance	Tugl
+VCKd	Vk	Commonality of Kedzudh	Gvur
+VDeG	Vd	Democracy of Greats	Knoe
+VDrN	VN	Drr'lana Network	Gash
+VDzF	Vf	Dzarrgh Federate	Dene/Prov/Tugl
+VFFD	V1	First Fleet of Dzo	Mesh
+VGoT	Vg	Glory of Taarskoerzn	Prov
+ViCo	Vi	Viyard Concourse	Gate
+VInL	V9	Infinity League	Knoe
+VIrM	Vh	Irrgh Manifest	Prov
+VJoF	Vj	Jihad of Faarzgaen	Prov
+VKfu	Vk	Kfue	Tugl
+VLIn	Vi	Llaeghskath Interacterate	Prov/Tugl
+VLPr	Vl	Lair Protectorate	Prov
+VNgC	Vn	Ngath Confederation	Wind
+VNoe	VN	Noefa	Tugl
+VOpA	Vo	Opposition Alliance	Knoe
+VOpp	Vo	Opposition Alliance	Mesh
+VOuz	VO	Ouzvothon	Tugl
+VPGa	Vg	Pact of Gaerr	Gvur
+VRo5	V5	Ruler of Five	Mesh
+VRrS	VW	Rranglloez Stronghold	Tugl
+VRuk	Vn	Worlds of Leader Rukh	Gvur
+VSDp	Vs	Saeknouth Dependency	Gvur
+VSEq	Vd	Society of Equals	Gvur/Tugl
+VThE	Vt	Thoengling Empire	Gvur/Tugl
+VTrA	VT	Trae Aggregation	Tren
+VTzE	Vp	Thirz Empire	Gvur/Ziaf
+VUru	Vu	Urukhu	Gvur
+VVar	Ve	Empire of Varroerth	Prov/Tugl/Wind
+VVoS	Vv	Voekhaeb Society	Mesh
+VWan	Vw	People of Wanz	Tugl
+VWP2	V2	Windhorn Pact of Two	Tugl
+VYoe	VQ	Union of Yoetyqq	Gash
+WiDe	Wd	Winston Democracy	Alde/Newo
+Wild	Wi	Wilds	various
+XXXX	Xx	Unknown	various
+ZePr	Zp	Zelphic Primacy	Farf
+ZhAx	Ax	Zhodani Consulate, Addaxur Reserve	Tien
+ZhCa	Ca	Zhodani Consulate, Colonnade Province	Vang/Farf
+ZhCh	Zh	Zhodani Consulate, Chtierabl Province	Chti
+ZhCo	Zh	Zhodani Consulate	various
+ZhIa	Zh	Zhodani Consulate, Iabrensh Province	Stia/Zdie
+ZhIN	Zh	Zhodani Consulate, Iadr Nsobl Province	Farf/Fore/Gvur/Spin/Yikl/Ziaf
+ZhJp	Zh	Zhodani Consulate, Jadlapriants Province	Tien/Zhda
+ZhMe	Zh	Zhodani Consulate, Meqlemianz Province	Eiap/Sidi/Eiap
+ZhOb	Zh	Zhodani Consulate, Obrefripl Province	various
+ZhSh	Zh	Zhodani Consulate, Shtochiadr Province	Itvi/Tlab
+ZhVQ	Zh	Zhodani Consulate, Vlanchiets Qlom Province	various
+ZiSi	Rv	Restored Vilani Imperium	
+Zuug	Zu	Zuugabish Tripartite	Mend
+ZyCo	Zc	Zydarian Codominium	Beyo
+Classic Traveller, MegaTraveller, Traveller: The New Era, Traveller: 4th Edition
+Older versions of Traveller use a 2-character code. Due to the limited number of useful 2-character codes, these are not unique across Charted Space. In addition to the secondary entries in the above table, the following codes are seen:
+
+Code	Description	Edition
+Dr	Droyne world.	
+Dd	Domain of Deneb	MegaTraveller.
+Fd	Federation of Daibei	MegaTraveller.
+Fi	Federation of Ilelish	MegaTraveller.
+La	League of Antares	MegaTraveller.
+Li	Lucan's Imperium	MegaTraveller.
+Ma	Margaret's Stronghold	MegaTraveller.
+Rv	Restored Vilani Empire/Ziru Sirka	MegaTraveller.
+St	Strephon's Imperium	MegaTraveller.
+Wi	Wilds	Traveller: The New Era.
+--	Empty, unclaimed system.	
