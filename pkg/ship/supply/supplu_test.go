3c3295e1097b3974eeb99b3d418778d146025b14
Galdoba <galdoba@mail.ru>
2021-11-30 20:45:20 +1000

    su

--- pkg/ship/supply/supplu_test.go
+++ pkg/ship/supply/supplu_test.go
@@ -1,1 +1,24 @@
 package supply
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSupply(t *testing.T) {
+	sc := NewSupplyCache(75000, 488)
+	//sc.add(CATEGORY_FOOD, 100)
+	sc.add(CATEGORY_MATERIALS_COMMON, 900)
+	sc.add(CATEGORY_MATERIALS_RARE, 50)
+	fmt.Println(sc.Remainder())
+	for i := -5; i < 1500; i++ {
+		sc := NewSupplyCache(75000, 488)
+		sc.add(CATEGORY_MATERIALS_COMMON, 900)
+		cons := sc.ConsumedForDay()
+		if cons > 100 || cons < 0 {
+			t.Errorf("consumed %v but expected 0-100 (input %v)\n", cons, i)
+		}
+	}
+	fmt.Println(sc.ConsumedForDay(), "% consumed")
+	fmt.Println(sc.Remainder())
+}
--- pkg/ship/supply/supply.go
+++ pkg/ship/supply/supply.go
@@ -1,7 +1,9 @@
 package supply
 
+import "fmt"
+
 const (
-	CATEGORY_FOOD = 0 iota
+	CATEGORY_FOOD = iota
 	CATEGORY_MATERIALS_COMMON
 	CATEGORY_MATERIALS_RARE
 	CATEGORY_BIOLOGICALS_RARE
@@ -18,42 +20,94 @@ const (
 
 type material struct {
 	category            int
-	capacity      int
+	capacity            int
 	currentConsumption  int
 	expectedConsumption int
 }
 
-
 type supplyCache struct {
-	byType 			map[int]*material
-	cacheVolume 	int
-	cargoConsumed 	int
+	byType          map[int]*material
+	cacheVolume     int
+	cargoConsumed   int
 	logisticsEffect int
+	shipTonnage     int
+	activeCrew      int
+	consumptionRate int
 }
 
-func NewSupplyCache(volume int) *supplyCache {
+func NewSupplyCache(tonnage, crew int) *supplyCache {
 	sc := supplyCache{}
 	sc.byType = make(map[int]*material)
-	sc.byType[CATEGORY_FOOD] = &material{CATEGORY_FOOD}
-	sc.byType[CATEGORY_MATERIALS_COMMON] = &material{CATEGORY_MATERIALS_COMMON}
-	sc.byType[CATEGORY_MATERIALS_RARE] = &material{CATEGORY_MATERIALS_RARE}
-	sc.byType[CATEGORY_BIOLOGICALS_RARE] = &material{CATEGORY_BIOLOGICALS_RARE}
-	sc.byType[CATEGORY_MATERIALS_EXOTIC] = &material{CATEGORY_MATERIALS_EXOTIC}
-	sc.cacheVolume = volume
+	sc.byType[CATEGORY_FOOD] = &material{CATEGORY_FOOD, 0, 0, 0}
+	sc.byType[CATEGORY_MATERIALS_COMMON] = &material{CATEGORY_MATERIALS_COMMON, 0, 0, 0}
+	sc.byType[CATEGORY_MATERIALS_RARE] = &material{CATEGORY_MATERIALS_RARE, 0, 0, 0}
+	sc.byType[CATEGORY_BIOLOGICALS_RARE] = &material{CATEGORY_BIOLOGICALS_RARE, 0, 0, 0}
+	sc.byType[CATEGORY_MATERIALS_EXOTIC] = &material{CATEGORY_MATERIALS_EXOTIC, 0, 0, 0}
+	sc.shipTonnage = tonnage
+	sc.cacheVolume = tonnage
+	sc.activeCrew = crew
+	sc.consumptionRate = 100
+
+	return &sc
+}
+
+func str(category int) string {
+	switch category {
+	case CATEGORY_FOOD:
+		return "Food              "
+	case CATEGORY_MATERIALS_COMMON:
+		return "Materials (Common)"
+	case CATEGORY_MATERIALS_RARE:
+		return "Materials (Rare)  "
+	case CATEGORY_BIOLOGICALS_RARE:
+		return "Biologicals (Rare)"
+	case CATEGORY_MATERIALS_EXOTIC:
+		return "Materials (Exotic)"
+	}
+	return "Unknown"
+}
+
+func (sc *supplyCache) add(category, volume int) {
+	sc.byType[category].capacity = sc.byType[category].capacity + volume
 }
 
 func (sc *supplyCache) Remainder() string {
-	list := []int{CATEGORY_FOOD,CATEGORY_MATERIALS_COMMON,CATEGORY_MATERIALS_RARE,CATEGORY_BIOLOGICALS_RARE,CATEGORY_MATERIALS_EXOTIC}
+	list := []int{CATEGORY_FOOD, CATEGORY_MATERIALS_COMMON, CATEGORY_MATERIALS_RARE, CATEGORY_BIOLOGICALS_RARE, CATEGORY_MATERIALS_EXOTIC}
 	total := 0
 	for _, val := range sc.byType {
-		total += val
+		total += val.capacity
 	}
-	r := "===SUPPLY REPORT============\n"
-	for i, val := range list {
-		switch i {
-		case CATEGORY_FOOD:
-			r += fmt.Sprintf("  Food: %v Units")
+	r := "===SUPPLY REPORT==================\n"
+	for _, category := range list {
+		switch sc.byType[category].capacity > 0 {
+		case true:
+			r += fmt.Sprintf("  %v: %v Units\n", str(category), sc.byType[category].capacity)
 		}
 	}
-	return ""
+	r += fmt.Sprintf("              Total: %v/%v SU\n", total, sc.cacheVolume)
+	r += "=================================="
+	return r
+}
+
+func (sc *supplyCache) ConsumedForDay() int {
+	perShip := sc.shipTonnage / 100
+	if sc.shipTonnage%100 > 0 {
+		perShip++
+	}
+	perCrew := sc.activeCrew/3 + 1
+	if sc.activeCrew%3 > 0 {
+		perCrew++
+	}
+	toConsume := perShip + perCrew
+	toConsume = (toConsume * sc.consumptionRate) / 100
+	switch toConsume <= sc.byType[CATEGORY_MATERIALS_COMMON].capacity {
+	case true:
+		sc.byType[CATEGORY_MATERIALS_COMMON].capacity = sc.byType[CATEGORY_MATERIALS_COMMON].capacity - toConsume
+		return sc.consumptionRate
+	case false:
+		canConsume := sc.byType[CATEGORY_MATERIALS_COMMON].capacity
+		sc.byType[CATEGORY_MATERIALS_COMMON].capacity = 0
+		return canConsume * 100 / toConsume
+	}
+	return 0
 }
