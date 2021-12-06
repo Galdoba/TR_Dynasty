47358ac5b2c5e13367a654600f2462bec73c99de
Galdoba <galdoba@mail.ru>
2021-12-06 19:43:52 +1000

    rep

--- pkg/ship/maintenance/maintenance.go
+++ pkg/ship/maintenance/maintenance.go
@@ -2,8 +2,10 @@ package maintenance
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/Galdoba/TR_Dynasty/pkg/dice"
+	"github.com/Galdoba/utils"
 )
 
 const (
@@ -13,31 +15,34 @@ const (
 	FAILURE
 	SYSTEM_STRUCTURE_HULL_minor
 	SYSTEM_STRUCTURE_HULL_major
-	SYSTEM_STRUCTURE_ARMOR
+	SYSTEM_STRUCTURE_ARMOR ///5
 	SYSTEM_STRUCTURE_CARGO
 	SYSTEM_ELECTRONICS_Navigation_Systems
 	SYSTEM_ELECTRONICS_COMBAT_SENSORS
 	SYSTEM_ELECTRONICS_MISSION_SENSORS
-	SYSTEM_ELECTRONICS_COMPUTER
+	SYSTEM_ELECTRONICS_COMPUTER ///10
 	SYSTEM_ELECTRONICS_BRIDGE_COMMAND
 	SYSTEM_ELECTRONICS_BRIDGE_ALL
 	SYSTEM_POWER_Powerplant_minor
 	SYSTEM_POWER_Powerplant_major
-	SYSTEM_DRIVES_Jump_Drive_minor
+	SYSTEM_DRIVES_Jump_Drive_minor /// 15
 	SYSTEM_DRIVES_Jump_Drive_major
 	SYSTEM_DRIVES_Manoeuvre_Drive_minor
 	SYSTEM_DRIVES_Manoeuvre_Drive_major
 	SYSTEM_WEAPON_Spinal_Weapon
-	SYSTEM_WEAPON_Secondary_Weapon
+	SYSTEM_WEAPON_Secondary_Weapon /// 20
 	SYSTEM_DEFENSIVE_Defensive_Systems
 	SYSTEM_DEFENSIVE_Craft_Bays_or_Drones
 	SYSTEM_GENERAL_Life_support
 	SYSTEM_GENERAL_Internal_Gravity
-	SYSTEM_GENERAL_Small_Craft
+	SYSTEM_GENERAL_Small_Craft /// 25
 	SYSTEM_GENERAL_Fuel_Processor
 	SYSTEM_GENERAL_Mission_System //laboratory, observatory or similar
 	SYSTEM_GENERAL_Systems_special
 	SYSTEM_DISTRIBUTION_ERROR
+	SCALE_MINOR
+	SCALE_MAJOR
+	SCALE_STRUCTURAL
 )
 
 type damageControlManager struct {
@@ -50,9 +55,10 @@ type DamageControlManager interface {
 	RepairCost() (int, int)
 }
 
-func NewDamageControlManager() *damageControlManager {
+func NewDamageControlManager(tonnage int) *damageControlManager {
 	dcm := damageControlManager{}
 	dcm.problemWith = make(map[int]problem)
+	dcm.shipTonnage = tonnage
 	return &dcm
 }
 
@@ -61,6 +67,8 @@ type problem struct {
 	breakdowns int
 	failure    int
 	system     int
+	scale      int
+	repairCost []int
 }
 
 //Assign - распределяет и скалирует проблемы с содержанием систем корабля
@@ -74,12 +82,76 @@ func (dcm *damageControlManager) Assign(issues ...int) error {
 		if problem.isVoid() {
 			problem = newProblem(system)
 		}
+
 		if err := problem.evaluate(issue); err != nil {
 			return fmt.Errorf("problem (%v) evaluate issue (%v): %v", problem, issue, err.Error())
 		}
+		problem.rollRepairCost(dcm.shipTonnage)
 		dcm.problemWith[system] = problem
+
 	}
 	return nil
+}
+
+func structuralList() []int {
+	return []int{
+		SYSTEM_STRUCTURE_HULL_minor,
+		SYSTEM_STRUCTURE_HULL_major,
+		SYSTEM_STRUCTURE_ARMOR,
+		SYSTEM_STRUCTURE_CARGO,
+	}
+
+}
+
+func majorSystemList() []int {
+	return []int{
+		SYSTEM_POWER_Powerplant_minor,
+		SYSTEM_POWER_Powerplant_major,
+		SYSTEM_DRIVES_Jump_Drive_minor,
+		SYSTEM_DRIVES_Jump_Drive_major,
+		SYSTEM_DRIVES_Manoeuvre_Drive_minor,
+		SYSTEM_DRIVES_Manoeuvre_Drive_major,
+		SYSTEM_WEAPON_Spinal_Weapon,
+		SYSTEM_WEAPON_Secondary_Weapon,
+		SYSTEM_DEFENSIVE_Defensive_Systems,
+		SYSTEM_DEFENSIVE_Craft_Bays_or_Drones,
+	}
+}
+
+func minorSystemList() []int {
+	return []int{
+		SYSTEM_ELECTRONICS_Navigation_Systems,
+		SYSTEM_ELECTRONICS_COMBAT_SENSORS,
+		SYSTEM_ELECTRONICS_MISSION_SENSORS,
+		SYSTEM_ELECTRONICS_COMPUTER,
+		SYSTEM_ELECTRONICS_BRIDGE_COMMAND,
+		SYSTEM_ELECTRONICS_BRIDGE_ALL,
+		SYSTEM_GENERAL_Life_support,
+		SYSTEM_GENERAL_Internal_Gravity,
+		SYSTEM_GENERAL_Small_Craft,
+		SYSTEM_GENERAL_Fuel_Processor,
+		SYSTEM_GENERAL_Mission_System,
+		SYSTEM_GENERAL_Systems_special,
+	}
+}
+
+func codeMatchScale(code int) int {
+	for _, val := range minorSystemList() {
+		if val == code {
+			return SCALE_MINOR
+		}
+	}
+	for _, val := range majorSystemList() {
+		if val == code {
+			return SCALE_MAJOR
+		}
+	}
+	for _, val := range structuralList() {
+		if val == code {
+			return SCALE_STRUCTURAL
+		}
+	}
+	return SYSTEM_DISTRIBUTION_ERROR
 }
 
 func newProblem(systemCode int) problem {
@@ -89,6 +161,7 @@ func newProblem(systemCode int) problem {
 		breakdowns: 0,
 		failure:    0,
 	}
+	pr.scale = codeMatchScale(systemCode)
 	return pr
 }
 
@@ -229,7 +302,8 @@ func (pr problem) String() string {
 	if pr.failure > 0 {
 		issue = "System Failed"
 	}
-	return fmt.Sprintf("[Problem {Code: %v = %v}]", pr.system, issue)
+	rep := fmt.Sprintf("Repair Cost: SU=%v/RM=%v/EM=%v/RT=%v", pr.repairCost[0], pr.repairCost[1], pr.repairCost[2], pr.repairCost[3])
+	return fmt.Sprintf("[Problem {Code: %v = %v; %v}]", pr.system, issue, rep)
 }
 
 func DetermineIssues(mods ...int) []int {
@@ -291,9 +365,106 @@ func DetermineIssues(mods ...int) []int {
 			}
 		}
 	}
-	s := "SU	Defect (minor)    : 2d * тоннаж/750 	Defect (major)    : 2d * тоннаж/150 	Breakdown (minor) : 5d * тоннаж/750 	Breakdown (major) : 5d * тоннаж/150 	Failure (minor)   : 8d * тоннаж/750 	Failure (major)   : 8d * тоннаж/150 	1 armor           : 2d * тоннаж/15 	1 HP              : 2d * 7 	hours 	из книги"
-	fmt.Print(s)
+	//s := "SU	Defect (minor)    : 2d * тоннаж/750 	Defect (major)    : 2d * тоннаж/150 	Breakdown (minor) : 5d * тоннаж/750 	Breakdown (major) : 5d * тоннаж/150 	Failure (minor)   : 8d * тоннаж/750 	Failure (major)   : 8d * тоннаж/150 	1 armor           : 2d * тоннаж/15 	1 HP              : 2d * 7 	hours 	из книги"
+	//fmt.Print(s)
 	return res
+}
+
+func (pr *problem) rollRepairCost(tons int) {
+	su := 0
+	rm := 0
+	em := 0
+	rt := 0
+	hpLim := tons * 40 / 100
+	v750 := utils.Max(tons/750, 1)
+	v150 := utils.Max(tons/150, 1)
+	v15 := utils.Max(tons/15, 1)
+	for i := 0; i < pr.defects; i++ {
+		switch pr.scale {
+		case SCALE_MINOR:
+			su += dice.Roll2D() * v750
+			rt = dice.Roll2D() * v750
+		case SCALE_MAJOR:
+			su += dice.Roll2D() * v150
+			rt = dice.Roll2D() * v150
+		}
+	}
+	for i := 0; i < pr.breakdowns; i++ {
+		switch pr.scale {
+		case SCALE_MINOR:
+			su += dice.Roll5D() * v750
+			rt = dice.New().RollNext("6d6").Sum() * v750
+			switch dice.Roll1D() {
+			case 1, 2, 3:
+				rm += dice.Roll3D()
+			case 4, 5, 6:
+				em += dice.Roll1D()
+			}
+		case SCALE_MAJOR:
+			su += dice.Roll5D() * v150
+			rt = dice.New().RollNext("20d6").Sum() * v150
+			switch dice.Roll1D() {
+			case 1, 2, 3:
+				rm += dice.New().RollNext("6d6").Sum()
+			case 4, 5, 6:
+				em += dice.Roll3D()
+			}
+		}
+	}
+	for i := 0; i < pr.failure; i++ {
+		switch pr.scale {
+		case SCALE_MINOR:
+			su += dice.New().RollNext("8d6").Sum() * v750
+			switch dice.Roll1D() {
+			case 1, 2, 3:
+				rm += dice.Roll4D()
+			case 4, 5, 6:
+				em += dice.Roll2D()
+			}
+		case SCALE_MAJOR:
+			su += dice.New().RollNext("8d6").Sum() * v150
+			switch dice.Roll1D() {
+			case 1, 2, 3:
+				rm += dice.New().RollNext("8d6").Sum()
+			case 4, 5, 6:
+				em += dice.Roll4D()
+			}
+		}
+	}
+	switch pr.system {
+	case SYSTEM_STRUCTURE_ARMOR, SYSTEM_STRUCTURE_CARGO:
+		d := pr.defects
+		b := dice.New().RollNext(strconv.Itoa(pr.breakdowns) + "d3").Sum()
+		f := dice.New().RollNext(strconv.Itoa(pr.failure) + "d6").Sum()
+		val := d + b + f
+		su += val * dice.Roll2D() * v15
+		switch dice.Roll1D() {
+		case 1, 2, 3:
+			rm += dice.Roll2D() * v750 * val
+		case 4, 5, 6:
+			em += dice.Roll1D() * v750 * val
+		}
+		rt = dice.Roll2D() * 50
+	case SYSTEM_STRUCTURE_HULL_minor, SYSTEM_STRUCTURE_HULL_major:
+		hp := 0
+		for m := 0; m < pr.defects; m++ {
+			hp += utils.Max(hpLim/100, 1)
+		}
+		for m := 0; m < pr.breakdowns; m++ {
+			hp += dice.New().RollNext("1d3").Sum() * hpLim / 100
+		}
+		for m := 0; m < pr.failure; m++ {
+			hp += dice.Roll1D() * hpLim / 100
+		}
+		if pr.system == SYSTEM_STRUCTURE_HULL_major {
+			hp = hp * 10
+		}
+		for j := 0; j < hp; j++ {
+			su += (dice.Roll2D() * 7)
+		}
+		rt = dice.Roll2D() * hp
+	}
+	pr.repairCost = append([]int{}, su, rm, em, rt)
 }
 
 /*
--- pkg/ship/maintenance/maintenance_test.go
+++ pkg/ship/maintenance/maintenance_test.go
@@ -6,12 +6,13 @@ import (
 )
 
 func TestReporter(t *testing.T) {
-	dcm := NewDamageControlManager()
+	screen()
+	dcm := NewDamageControlManager(75000)
 	fmt.Printf("%v\n", dcm)
 	if dcm == nil {
 		t.Errorf("dcm cannot ne NIL")
 	}
-	if err := dcm.Assign(DetermineIssues(40)...); err != nil {
+	if err := dcm.Assign(DetermineIssues(2)...); err != nil {
 		t.Errorf("Assign error: %v\n", err.Error())
 	} else {
 		fmt.Printf("defect assigned...\n")
@@ -21,4 +22,40 @@ func TestReporter(t *testing.T) {
 	for _, v := range dcm.problemWith {
 		fmt.Println(v)
 	}
+}
+
+func screen() {
+	fmt.Println("DEFECT =", DEFECT)
+	fmt.Println("BREAKDOWN =", BREAKDOWN)
+	fmt.Println("FAILURE =", FAILURE)
+	fmt.Println("SYSTEM_STRUCTURE_HULL_minor =", SYSTEM_STRUCTURE_HULL_minor)
+	fmt.Println("SYSTEM_STRUCTURE_HULL_major =", SYSTEM_STRUCTURE_HULL_major)
+	fmt.Println("SYSTEM_STRUCTURE_ARMOR =", SYSTEM_STRUCTURE_ARMOR)
+	fmt.Println("SYSTEM_STRUCTURE_CARGO =", SYSTEM_STRUCTURE_CARGO)
+	fmt.Println("SYSTEM_ELECTRONICS_Navigation_Systems =", SYSTEM_ELECTRONICS_Navigation_Systems)
+	fmt.Println("SYSTEM_ELECTRONICS_COMBAT_SENSORS =", SYSTEM_ELECTRONICS_COMBAT_SENSORS)
+	fmt.Println("SYSTEM_ELECTRONICS_MISSION_SENSORS =", SYSTEM_ELECTRONICS_MISSION_SENSORS)
+	fmt.Println("SYSTEM_ELECTRONICS_COMPUTER =", SYSTEM_ELECTRONICS_COMPUTER)
+	fmt.Println("SYSTEM_ELECTRONICS_BRIDGE_COMMAND =", SYSTEM_ELECTRONICS_BRIDGE_COMMAND)
+	fmt.Println("SYSTEM_ELECTRONICS_BRIDGE_ALL =", SYSTEM_ELECTRONICS_BRIDGE_ALL)
+	fmt.Println("SYSTEM_POWER_Powerplant_minor =", SYSTEM_POWER_Powerplant_minor)
+	fmt.Println("SYSTEM_POWER_Powerplant_major =", SYSTEM_POWER_Powerplant_major)
+	fmt.Println("SYSTEM_DRIVES_Jump_Drive_minor =", SYSTEM_DRIVES_Jump_Drive_minor)
+	fmt.Println("SYSTEM_DRIVES_Jump_Drive_major =", SYSTEM_DRIVES_Jump_Drive_major)
+	fmt.Println("SYSTEM_DRIVES_Manoeuvre_Drive_minor =", SYSTEM_DRIVES_Manoeuvre_Drive_minor)
+	fmt.Println("SYSTEM_DRIVES_Manoeuvre_Drive_major =", SYSTEM_DRIVES_Manoeuvre_Drive_major)
+	fmt.Println("SYSTEM_WEAPON_Spinal_Weapon =", SYSTEM_WEAPON_Spinal_Weapon)
+	fmt.Println("SYSTEM_WEAPON_Secondary_Weapon =", SYSTEM_WEAPON_Secondary_Weapon)
+	fmt.Println("SYSTEM_DEFENSIVE_Defensive_Systems =", SYSTEM_DEFENSIVE_Defensive_Systems)
+	fmt.Println("SYSTEM_DEFENSIVE_Craft_Bays_or_Drones =", SYSTEM_DEFENSIVE_Craft_Bays_or_Drones)
+	fmt.Println("SYSTEM_GENERAL_Life_support =", SYSTEM_GENERAL_Life_support)
+	fmt.Println("SYSTEM_GENERAL_Internal_Gravity =", SYSTEM_GENERAL_Internal_Gravity)
+	fmt.Println("SYSTEM_GENERAL_Small_Craft =", SYSTEM_GENERAL_Small_Craft)
+	fmt.Println("SYSTEM_GENERAL_Fuel_Processor =", SYSTEM_GENERAL_Fuel_Processor)
+	fmt.Println("SYSTEM_GENERAL_Mission_System =", SYSTEM_GENERAL_Mission_System)
+	fmt.Println("SYSTEM_GENERAL_Systems_special =", SYSTEM_GENERAL_Systems_special)
+	fmt.Println("SYSTEM_DISTRIBUTION_ERROR =", SYSTEM_DISTRIBUTION_ERROR)
+	fmt.Println("SCALE_MINOR =", SCALE_MINOR)
+	fmt.Println("SCALE_MAJOR =", SCALE_MAJOR)
+	fmt.Println("SCALE_STRUCTURAL =", SCALE_STRUCTURAL)
 }
